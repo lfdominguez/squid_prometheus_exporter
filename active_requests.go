158062b376b27dc680ec4ba4b4ebe7ca7cb92965
Luis Felipe Domínguez Vega <ldominguezvega@gmail.com>
2019-01-13 23:16:13 -0500

    Fix in URL parsing.
    Signed-off-by: Luis Felipe Domínguez Vega <ldominguezvega@gmail.com>

--- active_requests.go
+++ active_requests.go
@@ -76,7 +76,7 @@ func (collector *activeRequestsCollector) Collect(ch chan<- prometheus.Metric) {
 		tld := "unknown"
 		tldPlusOne := "unknown"
 
-		if parser, error := url.Parse(uri); error != nil {
+		if parser, error := url.Parse(uri); error == nil {
 			tld, _ = publicsuffix.PublicSuffix(parser.Hostname())
 			tldPlusOne, _ = publicsuffix.EffectiveTLDPlusOne(parser.Hostname())
 		}
